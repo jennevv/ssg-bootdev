ebdd41f412c97a11eba7d4db021b7748d42fef8a
jenne <jennevv@gmail.com>
2025-06-25 13:29:37 +0200

    add LeafNode and ParentNode subclasses

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,16 +14,56 @@ class HTMLNode:
         self.children = children
         self.props = props
 
-        if value is None and children is None:
-            raise ValueError("value and children cannot be None at the same time.")
-
     def to_html(self):
         raise NotImplementedError()
 
     def props_to_html(self):
         if self.props:
-            return f'href="{self.props["href"]}" target="{self.props["target"]}"'
+            return "".join([f' {key}="{value}"' for key, value in self.props.items()])
         return ""
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str,
+        value: str,
+        props: Optional[dict[str, str]] = None,
+    ):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.tag == "img":
+            return f"<{self.tag}{self.props_to_html()}>"
+        elif self.value is None:
+            raise ValueError("A LeafNode must have a value.")
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str,
+        children: HTMLNode,
+        props: Optional[dict[str, str]] = None,
+    ):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("A ParentNode must have a tag.")
+        if self.children is None:
+            raise ValueError("A ParentNode must have children.")
+
+        html = f"<{self.tag}>"
+        for child in self.children:
+            html += f"{child.to_html()}"
+        html += f"</{self.tag}>"
+
+        return html
--- unittests/test_htmlnode.py
+++ unittests/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.htmlnode import HTMLNode
+from src.htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -19,10 +19,6 @@ class TestHTMLNode(unittest.TestCase):
         with self.assertRaises(NotImplementedError):
             node.to_html()
 
-    def test_value_and_children_none(self):
-        with self.assertRaises(ValueError):
-            HTMLNode()
-
     def test_props_to_html(self):
         node = HTMLNode(
             "<a>",
@@ -32,9 +28,103 @@ class TestHTMLNode(unittest.TestCase):
         )
 
         self.assertEqual(
-            node.props_to_html(), 'href="https://www.google.com" target="_blank"'
+            node.props_to_html(), ' href="https://www.google.com" target="_blank"'
         )
 
         node2 = HTMLNode("<a>", "some text")
 
         self.assertEqual(node2.props_to_html(), "")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "www.boots.dev", "target": "_blank"})
+        self.assertEqual(
+            node.to_html(), '<a href="www.boots.dev" target="_blank">Click me!</a>'
+        )
+
+    def test_leaf_to_html_header(self):
+        node = LeafNode("h1", "header 1")
+        self.assertEqual(node.to_html(), "<h1>header 1</h1>")
+
+    def test_leaf_to_html_code(self):
+        node = LeafNode("code", "y = x")
+        self.assertEqual(node.to_html(), "<code>y = x</code>")
+
+    def test_to_html_img(self):
+        node = LeafNode(
+            "img",
+            None,
+            {
+                "src": "img_girl.jpg",
+                "alt": "Girl in a jacket",
+                "width": "500",
+                "height": "600",
+            },
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">',
+        )
+
+    def test_leaf_to_html_value_is_none(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_tag_is_none(self):
+        node = LeafNode(None, "some text")
+        self.assertEqual(node.to_html(), "some text")
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_child(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchild(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_no_children(self):
+        parent_node = ParentNode("div", None)
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
+
+    def test_to_html_with_children(self):
+        child_node1 = LeafNode("span", "child1")
+        child_node2 = LeafNode("b", "child2")
+        parent_node = ParentNode("div", [child_node1, child_node2])
+        self.assertEqual(
+            parent_node.to_html(), "<div><span>child1</span><b>child2</b></div>"
+        )
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node1a = LeafNode("b", "grandchild1a")
+        grandchild_node2a = LeafNode("b", "grandchild2a")
+        grandchild_node1b = LeafNode("b", "grandchild1b")
+        grandchild_node2b = LeafNode("b", "grandchild2b")
+        child_node1 = ParentNode("span", [grandchild_node1a, grandchild_node1b])
+        child_node2 = ParentNode("span", [grandchild_node2a, grandchild_node2b])
+        parent_node = ParentNode("div", [child_node1, child_node2])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild1a</b><b>grandchild1b</b></span><span><b>grandchild2a</b><b>grandchild2b</b></span></div>",
+        )
+
+    def test_to_html_with_nested_parents(self):
+        parent_node1 = ParentNode("div", [])
+        parent_node2 = ParentNode("div", [parent_node1])
+        parent_node1.children = [parent_node2]
+        with self.assertRaises(RecursionError):
+            parent_node1.to_html()
