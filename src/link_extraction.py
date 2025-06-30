374679d65298c7bed982a89f66ce6ba49060aa36
jenne <jennevv@gmail.com>
2025-06-30 12:25:13 +0200

    fix output type hints

--- src/link_extraction.py
+++ src/link_extraction.py
@@ -1,11 +1,11 @@
 import re
 
 
-def extract_markdown_images(text: str) -> list[tuple[str]]:
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     image = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
     return image
 
 
-def extract_markdown_links(text: str) -> list[tuple[str]]:
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     link = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
     return link
